36fee700f8873880a4cf7d395ac1d32f8c320ce8
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-05 23:31:49 +0530

    pass 6th stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+fn is_builtin(command: &str) -> bool {
+    match command {
+        "echo" | "exit" | "type" => true,
+        _ => false,
+    }
+}
+
+fn is_executable(command: &str) -> Option<String> {
+    // Check if the command is available in the system's PATH
+    if let Some(paths) = std::env::var_os("PATH") {
+        for path in std::env::split_paths(&paths) {
+            let full_path = path.join(command);
+            if full_path.is_file() {
+                // Return the full path to the executable
+                return Some(full_path.to_string_lossy().into_owned());
+            }
+        }
+    }
+    None
+}
 fn main() {
     loop {
         print!("$ ");
@@ -26,13 +46,12 @@ fn main() {
                     println!("type: argument required");
                 } else {
                     for arg in args {
-                        match arg {
-                            "echo" | "exit" | "type" => {
-                                println!("{} is a shell builtin", arg);
-                            }
-                            _ => {
-                                println!("{} not found", arg);
-                            }
+                        if is_builtin(arg) {
+                            println!("{} is a shell builtin", arg);
+                        } else if let Some(executable_path) = is_executable(arg) {
+                            println!("{} is {}", arg, executable_path);
+                        } else {
+                            println!("{}: command not found", arg);
                         }
                     }
                 }
